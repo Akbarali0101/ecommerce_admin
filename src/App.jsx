2080f24738736b7f52a05c4f9b8cebc7b2bee816
Togayev8 <togayevqahramon88@gmail.com>
2026-07-29 15:29:17 +0500

    temp commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AdminLayout } from "@/components/layout/AdminLayout";
-import { ProtectedAdminRoute } from "@/pages/auth/ProtectedAdminRoute";
 
 import AdminLogin from "@/pages/auth/AdminLogin";
 
@@ -12,13 +11,10 @@ import AdminOrders from "@/pages/admin/Orders";
 import AdminBanners from "@/pages/admin/Banners";
 import Users from "@/pages/admin/Users";
 import Settings from "@/pages/admin/Settings";
-import AdminUsers from "@/pages/admin/Users";
-import NotFound from "@/pages/NotFound";
 
 function App() {
   return (
     <Routes>
-      {/* Auth */}
       <Route path="/login" element={<AdminLogin />} />
 
       <Route path="/" element={<AdminLayout />}>
@@ -31,20 +27,9 @@ function App() {
         <Route path="banners" element={<AdminBanners />} />
         <Route path="users" element={<Users />} />
         <Route path="settings" element={<Settings />} />
-      {/* Admin panel - faqat token bo'lganlar kira oladi */}
-      <Route element={<ProtectedAdminRoute />}>
-        <Route path="/" element={<AdminLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="products" element={<AdminProducts />} />
-          <Route path="products/new" element={<ProductForm />} />
-          <Route path="products/:id" element={<ProductForm />} />
-          <Route path="categories" element={<AdminCategories />} />
-          <Route path="orders" element={<AdminOrders />} />
-          <Route path="banners" element={<AdminBanners />} />
-          <Route path="users" element={<AdminUsers />} />
-        </Route>
       </Route>
-      <Route path="*" element={<NotFound />} />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
